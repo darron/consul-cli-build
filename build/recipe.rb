bf507dedfb2d95cdf124037e7e513b965c11cd89
darron froese <darron@froese.org>
2016-08-02 15:35:41 -0600

    Update to 0.3.1

--- build/recipe.rb
+++ build/recipe.rb
@@ -1,13 +1,13 @@
 class Consulcli < FPM::Cookery::Recipe
   name 'consul-cli'
 
-  version '0.2.0'
+  version '0.3.1'
   revision '1'
   description 'CLI to query and manage Consul.'
 
   homepage 'https://github.com/CiscoCloud/consul-cli'
   source "https://github.com/CiscoCloud/consul-cli/releases/download/v#{version}/consul-cli_#{version}_linux_amd64.tar.gz"
-  sha256 '0282b3a76c642cb7b541c53254d0d847aba083b7ae586e1fbfba5c83370715e2'
+  sha256 '037150d3d689a0babf4ba64c898b4497546e2fffeb16354e25cef19867e763f1'
 
   maintainer 'Darron Froese <darron@froese.org>'
   vendor 'octohost'
